4069f1819eb5a6a745436970aec8c213d1c39b77
Ryan Southard <eodchop@gmail.com>
2017-06-29 20:18:16 -0500

    Logic

---
+++ paperRockScissors.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ryansouthard on 6/29/17.
+ */
+
+
+var options = ["rock", "paper", "scissors"];
+var chosenOne = Math.floor(Math.random(options) * 3);
+console.log(options[chosenOne]);
