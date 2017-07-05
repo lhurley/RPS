cbc41d964b2c9e5e0f029ca45120aa1d19e54789
Lizzie Hurley <l.hurley58@gmail.com>
2017-07-05 15:02:28 -0500

    initial site files

--- paperRockScissors.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ryansouthard on 6/29/17.
- */
-
-
-var options = ["rock", "paper", "scissors"];
-var chosenOne = Math.floor(Math.random(options) * 3);
-console.log(options[chosenOne]);
